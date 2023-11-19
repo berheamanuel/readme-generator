b60dfd1cd3465165bc171713963c5d1c03dca21e
Amanuel <amanueleman@gmail.com>
2023-11-18 22:25:49 -0500

    Added rendering license link handling function to markdown.

--- assests/js/generateMarkdown.js
+++ assests/js/generateMarkdown.js
@@ -31,7 +31,42 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = '';
+  // select correct license link for the selected license
+  switch (license) {
+    case 'GNU GPL v3':
+      licenseLink = 'https://www.gnu.org/licenses/gpl-3.0';
+      break;
+    case 'GNU GPL v2':
+      licenseLink = 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html';
+      break;
+    case 'GNU AGPL v3':
+      licenseLink = 'https://www.gnu.org/licenses/agpl-3.0';
+      break;
+    case 'GNU LGPL v3':
+      licenseLink = 'https://www.gnu.org/licenses/lgpl-3.0';
+      break;
+    case 'GNU FDL v1.3':
+      licenseLink = 'https://www.gnu.org/licenses/fdl-1.3';
+      break;
+    case 'The MIT License':
+      licenseLink = 'https://opensource.org/licenses/MIT';
+      break;
+    case 'Mozilla Public License 2.0':
+      licenseLink = 'https://opensource.org/licenses/MPL-2.0';
+      break;
+    case 'Boost Software License 1.0':
+      licenseLink = 'https://www.boost.org/LICENSE_1_0.txt';
+      break;
+    case 'Apache 2.0 License':
+      licenseLink = 'https://opensource.org/licenses/Apache-2.0'
+    default:
+      licenseLink = "";
+      break;
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
